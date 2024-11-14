d5a7b6647b07cf416e4a37e8efb5b0392fc36108
Adnan Zaki <adnzaki@outlook.com>
2024-11-14 10:05:04 +0700

    feat: add command runner to anticipate if the hosting provider does not support terminal usage

---
+++ api/app/Controllers/CommandRunner.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class CommandRunner extends BaseController
+{
+    public function runCommand()
+    {
+        $command = $this->request->getPost(['command', 'username', 'password']);
+        if($command['username'] === env('dev_username') && $command['password'] === env('dev_password')) {
+            return $this->response->setJSON(command($command['command']));
+        }
+    }
+}
