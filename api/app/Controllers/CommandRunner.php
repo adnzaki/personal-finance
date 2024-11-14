5c295755d78df77fd318b64b6917b9718b5a01ba
Adnan Zaki <adnzaki@outlook.com>
2024-11-14 10:07:02 +0700

    [command runner]: add message for invalid username or password

--- api/app/Controllers/CommandRunner.php
+++ api/app/Controllers/CommandRunner.php
@@ -11,6 +11,8 @@ class CommandRunner extends BaseController
         $command = $this->request->getPost(['command', 'username', 'password']);
         if($command['username'] === env('dev_username') && $command['password'] === env('dev_password')) {
             return $this->response->setJSON(command($command['command']));
+        } else {
+            return $this->response->setJSON(['status' => 'failed', 'message' => 'Invalid username or password']);
         }
     }
 }
