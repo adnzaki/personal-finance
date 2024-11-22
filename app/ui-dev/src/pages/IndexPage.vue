d4c931e5876739ef12d8f8b51a7e5dc7949002a7
Adnan Zaki <adnzaki@outlook.com>
2024-11-22 13:23:31 +0700

    added app logo and icon!

--- app/ui-dev/src/pages/IndexPage.vue
+++ app/ui-dev/src/pages/IndexPage.vue
@@ -2,14 +2,14 @@
   <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
+      src="~assets/main-logo.png"
       style="width: 200px; height: 200px"
-    >
+    />
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
-});
+  name: 'IndexPage',
+})
 </script>
