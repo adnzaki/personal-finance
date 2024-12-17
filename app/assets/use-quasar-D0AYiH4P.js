79a8f09aaec8be1963aafccb3cd1455e8723be1d
Adnan Zaki <adnzaki@outlook.com>
2024-12-17 16:00:57 +0700

    publish hotfix

---
+++ app/assets/use-quasar-D0AYiH4P.js
@@ -0,0 +1,1 @@
+import{i as a,bz as r}from"./index-BZZT44KJ.js";function u(){return a(r)}export{u};
--- app/assets/use-quasar-DDQxJQOd.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bz as r}from"./index-D-6t3Skm.js";function u(){return a(r)}export{u};
