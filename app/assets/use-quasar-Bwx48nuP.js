efb7d7de0c5bc44d4726ebe4c0fa778c46391b3a
Adnan Zaki <adnzaki@outlook.com>
2024-12-25 01:07:07 +0700

    publish the latest fix

--- app/assets/use-quasar-Bwx48nuP.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bE as r}from"./index-CiC5vzQS.js";function u(){return a(r)}export{u};
---
+++ app/assets/use-quasar-dzG0vDdZ.js
@@ -0,0 +1,1 @@
+import{i as a,bE as r}from"./index-CDT5vLCL.js";function u(){return a(r)}export{u};
