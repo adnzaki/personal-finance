5ed80064e214b57c92f672bef3d90c50e435170f
Adnan Zaki <adnzaki@outlook.com>
2025-03-05 14:47:02 +0700

    Revert "remove all build files"
    This reverts commit 5a34dd9240610fdbbd9321151c3324681147c177.

---
+++ app/assets/fab-jDNikWek.js
@@ -0,0 +1,1 @@
+import{r as s,aA as a}from"./index-BEae-QxD.js";a.lt.sm;const e=s([20,20]);s(!1);export{e as f};
---
+++ app/assets/use-quasar-D4Rp_hc1.js
@@ -0,0 +1,1 @@
+import{a,bE as r}from"./index-BEae-QxD.js";function u(){return a(r)}export{u};
