f7546b9fc2ae83b5432ea8c98ff189f803203738
Adnan Zaki <adnzaki@outlook.com>
2025-03-21 08:35:11 +0700

    update changelog

--- app/ui-dev/src/boot/app.js
+++ app/ui-dev/src/boot/app.js
@@ -1,6 +1,6 @@
 import { defineBoot } from '#q-app/wrappers'
 
-const appVersion = '1.0.0-alpha.8'
+const appVersion = '1.0.0-alpha.9'
 
 export default defineBoot(({ app }) => {
   app.config.globalProperties.$appVersion = appVersion
